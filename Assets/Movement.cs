9a2168544f5c922beac0f52c4e825cf6133b40f1
oiver12 <36069780+oiver12@users.noreply.github.com>
2020-09-07 13:46:24 +0200

    Physics

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		//transform.position = new Vector3(5f, 10f, 0f);
+
+	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
