30d4e012ac51e81a3f9305f4e2753cc329a53ef3
oiver12 <36069780+oiver12@users.noreply.github.com>
2020-09-09 17:19:52 +0200

    Background

---
+++ Assets/BackgroundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+
+	public GameObject background;
+	GameObject background1;
+	GameObject activeBackground;
+	public static BackgroundManager instance;
+	float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		instance = this;
+		background1 = Instantiate(background);
+		background1.SetActive(false);
+		activeBackground = background;
+		speed = MovementTiles.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		activeBackground.transform.Translate(new Vector3(-speed * Time.deltaTime, 0f, 0f));
+		if (activeBackground.transform.position.x <= 0f)
+			ShowNewBackground();
+    }
+
+	void ShowNewBackground()
+	{
+
+	}
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,16 @@ public class CameraController : MonoBehaviour
 	public Transform m_Target;
 	float currentVel;
 	public float smoothTime;
+	float yOffset;
+
+	private void Start()
+	{
+		yOffset = m_Target.position.y - transform.position.y;
+	}
 
 	void Update()
 	{
 		float posy = Mathf.SmoothDamp(transform.position.y, m_Target.position.y, ref currentVel, smoothTime);
-		transform.position = new Vector3(transform.position.x, posy, transform.position.z);
+		transform.position = new Vector3(transform.position.x, posy - yOffset, transform.position.z);
 	}
 }
